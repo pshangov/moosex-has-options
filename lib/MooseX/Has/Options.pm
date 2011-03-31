4c2f5d21ac090d1824684d5ee0bda8466a5a7f6e
Peter Shangov <pshangov@yahoo.com>
2011-03-31 18:56:52 +0300

    Removed windows newlines and tabs

--- lib/MooseX/Has/Options.pm
+++ lib/MooseX/Has/Options.pm
@@ -10,50 +10,50 @@ use Carp           ();
 
 sub import
 {
-	my ($class, @keywords) = @_;
-	@keywords = 'has' unless @keywords;
-	my $stash = Package::Stash->new(scalar caller());
+    my ($class, @keywords) = @_;
+    @keywords = 'has' unless @keywords;
+    my $stash = Package::Stash->new(scalar caller());
 
-	foreach my $keyword (@keywords)
-	{
-		if ($stash->has_symbol("&$keyword"))
-		{
-			my $orig = $stash->get_symbol("&$keyword");
-			$stash->add_symbol("&$keyword", sub { $orig->(_expand_options($keyword, @_)) });
-		}
-		else
-		{
+    foreach my $keyword (@keywords)
+    {
+        if ($stash->has_symbol("&$keyword"))
+        {
+            my $orig = $stash->get_symbol("&$keyword");
+            $stash->add_symbol("&$keyword", sub { $orig->(_expand_options($keyword, @_)) });
+        }
+        else
+        {
             Carp::carp "Cannot add options for $keyword, no subroutine found in caller package";
-		}
-	}
+        }
+    }
 }
 
 sub _expand_options
 {
-	my $keyword = shift;
+    my $keyword = shift;
     my $name = shift;
 
-	my %expanded;
+    my %expanded;
 
-	foreach my $option (@_)
-	{
-		if ( $keyword eq 'has' and $option =~ /^:(ro|rw|bare)$/ )
-		{
-			$expanded{is} = $1;
-		}
-		elsif ( $option =~ /^:(\w+)$/ )
-		{
-			$expanded{$1} = 1;
-		}
-		else
-		{
-			last;
-		}
-	}
-	
-	splice @_, 0, scalar keys %expanded;
+    foreach my $option (@_)
+    {
+        if ( $keyword eq 'has' and $option =~ /^:(ro|rw|bare)$/ )
+        {
+            $expanded{is} = $1;
+        }
+        elsif ( $option =~ /^:(\w+)$/ )
+        {
+            $expanded{$1} = 1;
+        }
+        else
+        {
+            last;
+        }
+    }
 
-	return $name, @_, %expanded;
+    splice @_, 0, scalar keys %expanded;
+
+    return $name, @_, %expanded;
 }
 
 1;
@@ -85,7 +85,7 @@ This module provides a succinct syntax for declaring options for L<Moose> attrib
 
 =head2 Declaring options
 
-MooseX::Has::Params works by checking the arguments to C<has> for strings that look like options, i.e. alphanumeric strings preceded by a colon, and replaces them with a hash whose keys are the names of the options (sans the colon) and the values are C<1>'s. Thus, 
+MooseX::Has::Params works by checking the arguments to C<has> for strings that look like options, i.e. alphanumeric strings preceded by a colon, and replaces them with a hash whose keys are the names of the options (sans the colon) and the values are C<1>'s. Thus,
 
     has 'some_attribute', ':required';
 
@@ -116,7 +116,7 @@ If you specify arguments when importing MooseX::Has::Params, it will hook to the
         qw(:required),
         type => 'Text',
     );
-    
+
 The special treatment of C<ro>, C<rw> and C<bare> will be disabled for such functions.
 
 =head1 SEE ALSO
